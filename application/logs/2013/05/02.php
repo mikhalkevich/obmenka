a53f617c2b0d895f42bd2f5820c9f55eecd6b74c
Mikhalkevich <mikhalkevich@ya.ru>
2013-05-16 20:30:32 +0300

    фыва

---
+++ application/logs/2013/05/02.php
@@ -0,0 +1,22 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-05-02 20:28:14 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-02 20:28:14 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-02 20:28:14 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-02 20:28:14 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-02 20:28:14 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-02 20:28:14 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-02 20:28:14 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-02 20:28:14 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
