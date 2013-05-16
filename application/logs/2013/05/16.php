1af5f6c44a97978eee3766596a0496659241f815
Mikhalkevich <mikhalkevich@ya.ru>
2013-05-16 21:45:57 +0300

    еуые

---
+++ application/logs/2013/05/16.php
@@ -0,0 +1,128 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-05-16 19:54:46 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 19:54:46 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 19:54:46 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 19:54:46 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 19:54:46 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 19:54:46 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 19:54:46 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 19:54:46 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:53 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/520/s_f100050_2012_11_20_01_45.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:53 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/520/s_f100050_2012_11_20_01_45.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/img/kot.png ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/img/kot.png ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/593/s_u40055393_2012_11_28_07_48.jpeg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/593/s_u40055393_2012_11_28_07_48.jpeg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/426/s_n139715_2012_12_22_05_13.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f86190_2012_11_30_03_26.JPG ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/702/s_113487_1005_2012_12_13_01_13.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/426/s_n139715_2012_12_22_05_13.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/702/s_113487_1005_2012_12_13_01_13.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/1060/s_f102809_2012_12_03_07_34.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f86190_2012_11_30_03_26.JPG ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/417/s_40045372_9995_2012_11_19_11_33.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/1060/s_f102809_2012_12_03_07_34.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/593/s_u40060387_2013_01_03_08_11.jpeg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/417/s_40045372_9995_2012_11_19_11_33.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/593/s_u40060387_2013_01_03_08_11.jpeg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/210/s_f72000_2012_12_19_11_23.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/210/s_f72000_2012_12_19_11_23.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/179/s_f100899_2012_11_19_10_23.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/179/s_f100899_2012_11_19_10_23.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/426/s_139715_1005_2012_12_20_06_10.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/426/s_139715_1005_2012_12_20_06_10.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/872/s_f101314_2012_12_07_03_41.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/872/s_f101314_2012_12_07_03_41.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/733/s_40067238_9995_2012_11_30_04_07.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/733/s_40067238_9995_2012_11_30_04_07.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/208/s_f94223_2012_11_28_05_55.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/users/small_26_1361619537.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/users/small_26_1361619537.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/208/s_f94223_2012_11_28_05_55.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/815/s_f84613_2012_11_26_06_04.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/519/s_f85507_2012_11_28_02_50.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/519/s_f85507_2012_11_28_02_50.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/198/s_f97233_2012_12_20_06_57.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/815/s_f84613_2012_11_26_06_04.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/1881/s_8-992213_2012_12_22_01_57.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/198/s_f97233_2012_12_20_06_57.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:55 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/users/small_44_1362481245.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-16 21:14:55 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/users/small_44_1362481245.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:55 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/960/s_n146691_2013_01_08_03_21.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-16 21:14:54 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/1881/s_8-992213_2012_12_22_01_57.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
