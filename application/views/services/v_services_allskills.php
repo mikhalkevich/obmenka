b9c02a6cbe88d37e55e99808c7154d46ff92e493
mikhalkevich <mikhalkevich@ya.ru>
2013-05-17 14:42:26 +0200

    16.05.13

---
+++ application/views/services/v_services_allskills.php
@@ -0,0 +1,15 @@
+<div class="mytable_account">
+    <?foreach($all_skills as $skill):?>
+        <?= HTML::anchor('#', $skill->skills->skill);?>
+
+        <?if(isset($skill->users->usermore->pic)):?>
+            <?$picture = 'media/uploads/users/small_'.$skill->users->usermore->pic?>
+        <?else:?>
+            <?$picture = 'media/img/user.png'?>
+        <?endif;?>
+     <?=HTML::image($picture, array(
+                 'width'=>'50px',
+                 'align'=>'left'));?>
+    <hr style="clear:both"/>
+    <?endforeach;?>
+</div>
