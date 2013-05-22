5af5b73a1f042918bd3bfcd873bebfb7e1578042
Mikhalkevich <mikhalkevich@ya.ru>
2013-05-22 23:57:56 +0300

    test

---
+++ application/logs/2013/05/22.php
@@ -0,0 +1,32 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2013-05-22 23:32:45 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-22 23:32:45 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-22 23:32:45 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/eshop/877/s_f81248_2012_11_21_05_12.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-22 23:32:45 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364372133.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-22 23:32:45 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-22 23:32:45 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/70/small_70_1364572959.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-22 23:32:45 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-22 23:32:45 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/uploads/tovars/2/small_2_1364366825.jpg ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-22 23:32:59 --- ERROR: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/css/indicator.gif ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+2013-05-22 23:32:59 --- STRACE: HTTP_Exception_404 [ 404 ]: Unable to find a route to match the URI: media/css/indicator.gif ~ SYSPATH\classes\kohana\request.php [ 1126 ]
+--
+#0 C:\xampp\htdocs\obmenka.by\index.php(109): Kohana_Request->execute()
+#1 {main}
+2013-05-22 23:33:27 --- ERROR: ErrorException [ 1 ]: Class 'Model_Ebook' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+2013-05-22 23:33:27 --- STRACE: ErrorException [ 1 ]: Class 'Model_Ebook' not found ~ MODPATH\orm\classes\kohana\orm.php [ 37 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
